bea02e95be739ce1f9a12c00e6993ea9d486218e
Honza Dvorsky <jan.dvorsky@yahoo.com>
2016-06-03 11:36:29 +0200

    Simplified package.swift

--- Package.swift
+++ Package.swift
@@ -1,17 +1,5 @@
 import PackageDescription
 
 let package = Package(
-  name: "Environment",
-  exclude: [],
-  targets: [
-    Target(
-      name: "Environment"
-    ),
-    Target(
-      name: "EnvironmentTests",
-      dependencies: [
-        .Target(name: "Environment")
-      ]
-    )
-  ]
+  name: "Environment"
 )
