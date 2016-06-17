3e59e928187e438a356c23ffe7b6d3a7bac40e50
Dan Appel <dan.appel00@gmail.com>
2016-06-17 14:27:45 -0700

    Make function on Environment static

--- Tests/Environment/EnvironmentTests.swift
+++ Tests/Environment/EnvironmentTests.swift
@@ -5,22 +5,22 @@ import XCTest
 class EnvironmentTests: XCTestCase {
 
     func testGet() {
-        guard let path = Environment().getVar("PATH") else { XCTFail(); return }
+        guard let path = Environment.getVar("PATH") else { XCTFail(); return }
         XCTAssert(!path.isEmpty)
     }
 
     func testSetGet() {
-        Environment().setVar("KITTENS", value: "FUZZY")
-        let val = Environment().getVar("KITTENS")
+        Environment.setVar("KITTENS", value: "FUZZY")
+        let val = Environment.getVar("KITTENS")
         XCTAssertEqual(val, "FUZZY")
     }
 
     func testRemove() {
-        Environment().setVar("MOARKITTENS", value: "FUZZIER")
-        let val = Environment().getVar("MOARKITTENS")
+        Environment.setVar("MOARKITTENS", value: "FUZZIER")
+        let val = Environment.getVar("MOARKITTENS")
         guard val == "FUZZIER" else { XCTFail(); return }
-        Environment().removeVar("MOARKITTENS")
-        let valFinal = Environment().getVar("MOARKITTENS")
+        Environment.removeVar("MOARKITTENS")
+        let valFinal = Environment.getVar("MOARKITTENS")
         XCTAssertNil(valFinal)
     }
 }
